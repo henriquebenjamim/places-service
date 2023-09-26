984731886d1e77ca7e5635f237ecb7975e124629
henriquebenjamim <henriiqqq@gmail.com>
2023-09-26 20:30:15 -0300

    Starting repository with initial config by spring initializr

---
+++ src/test/java/com/henriquebenjamim/placesapi/PlacesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquebenjamim.placesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
