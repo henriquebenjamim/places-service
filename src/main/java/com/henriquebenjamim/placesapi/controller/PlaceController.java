d4019b0f5846de4538d63dcd5b661f708b8674cd
henriquebenjamim <henriiqqq@gmail.com>
2023-10-04 16:02:01 -0300

    Feat: updated update method and Post method was implemented DateTime fields on create and update

--- src/main/java/com/henriquebenjamim/placesapi/controller/PlaceController.java
+++ src/main/java/com/henriquebenjamim/placesapi/controller/PlaceController.java
@@ -1,12 +1,16 @@
 package com.henriquebenjamim.placesapi.controller;
 
+import com.henriquebenjamim.placesapi.exceptions.ResourceNotFoundException;
 import com.henriquebenjamim.placesapi.models.Place;
 import com.henriquebenjamim.placesapi.services.PlaceService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -17,8 +21,12 @@ public class PlaceController {
     private PlaceService placeService;
 
     @PostMapping
-    public Place create(@RequestBody @Valid Place place) {
-        return placeService.create(place);
+    public ResponseEntity<Place> create(@RequestBody @Valid Place place) {
+        Place newPlace = placeService.create(place);
+        newPlace.setCreatedAt(LocalDateTime.now());
+        newPlace.setUpdatedAt(LocalDateTime.now());
+
+        return new ResponseEntity<>(newPlace, HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -27,8 +35,18 @@ public class PlaceController {
     }
 
     @PutMapping("{id}")
-    public Place update(@PathVariable Long id, @RequestBody Place place) {
-        return placeService.update(place);
+    public ResponseEntity<Place> updatePlace(@PathVariable Long id, @RequestBody Place placeDetails) {
+        Place updatedPlace = placeService.getById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Place not exist with id: " + id));
+
+        updatedPlace.setName(placeDetails.getName());
+        updatedPlace.setSlug(placeDetails.getSlug());
+        updatedPlace.setCity(placeDetails.getCity());
+        updatedPlace.setState(placeDetails.getState());
+        updatedPlace.setUpdatedAt(LocalDateTime.now());
+
+        placeService.update(updatedPlace);
+        return ResponseEntity.ok(updatedPlace);
     }
 
     @DeleteMapping("{id}")
